a761ffdc37e55b19f9f7994ae566fcdfd87a7ee4
ipaschos <ipaschos@stud.macromedia.de>
2021-02-08 12:31:47 +0100

    added database model

--- pepowide/pepowidehard/models.py
+++ pepowide/pepowidehard/models.py
@@ -1,3 +1,6 @@
 from django.db import models
 
-# Create your models here.
+class Basictest(models.Model):
+    full_name = models.CharField(max_length=70)
+    def __str__(self):
+        return self.full_name
