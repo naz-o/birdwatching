306a34b9eeb17d3c3fbb0b460b1129f3579eedb2
KaeseKrusten <fail-mail-fail@web.de>
2021-02-15 13:33:35 +0100

    views.py buzzing

--- project/pepowidehard/views.py
+++ project/pepowidehard/views.py
@@ -1,8 +1,9 @@
 from django.shortcuts import render
 from django.http import HttpResponse
-
 from .models import djangodb
+from gpiozero import Buzzer
 
+buzzer = Buzzer(17)
 all = djangodb.objects.all()
 data= {
     "data": all
@@ -16,7 +17,9 @@ def data_view(request):
 def htag(request):
     return render(request,"main.html")
 def buzzing(request):
-    #buzzer.toggle()
-    print("bruh")
+    buzzer.on()
+    sleep(1)
+    buzzer.off()
+    sleep(1)
     return render(request,"main.html")
 print("bruh")
