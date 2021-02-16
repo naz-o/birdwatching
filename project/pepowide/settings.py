eb0a7b70cdca476cd4e3c27a4c77a4b874162aed
KaeseKrusten <fail-mail-fail@web.de>
2021-02-16 12:33:51 +0100

    allowed host = 0.0.0.0

--- project/pepowide/settings.py
+++ project/pepowide/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '0%f$+fjcwztq!+-4)hy+y7kre96fgzq9+(@9yucynsczi4#!ht'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ["127.0.0.1"]
+ALLOWED_HOSTS = ["0.0.0.0"]
 
 
 # Application definition
--- project/pepowidehard/views.py
+++ project/pepowidehard/views.py
@@ -1,10 +1,12 @@
 from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from .models import djangodb
-#from gpiozero import Buzzer
+"""
+from gpiozero import Buzzer
 from time import sleep
+buzzer = Buzzer(17)
+"""
 
-#buzzer = Buzzer(17)
 all = djangodb.objects.all()
 data= {
     "data": all
